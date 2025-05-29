a9977bea53848fa9f92607f7520e30eeed8d905d
William <wmario2004@gmail.com>
2025-05-29 16:16:28 -0400

    Add initial project structure and resources for SplitPayapp
    - Created AboutAssets.txt for raw assets deployment.
    - Added splash screen SVG for application branding.
    - Defined color resources in Colors.xaml for consistent theming.
    - Established styles in Styles.xaml for various UI components.
    - Configured SplitPayapp.csproj with necessary properties and references.

---
+++ SplitPayapp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SplitPayapp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SplitPayapp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace SplitPayapp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Createhousehold(object sender, EventArgs e)
+	{
+		
+    }
+}
+
