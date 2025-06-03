9083d2c8721ccada2f5f768163c0f3f96a31f00e
William <wmario2004@gmail.com>
2025-06-03 14:21:43 -0400

    Refactor project to support multi-targeting for Android and iOS, update dependencies, and remove obsolete files.

--- Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
+++ Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+209d9157e266e1ace36cffb05248211141d8787c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f74f6ffbd5ea144f2544c8314884ec7f69f239ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
