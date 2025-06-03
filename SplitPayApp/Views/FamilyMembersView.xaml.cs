507d6fbeba878bd33ff019afd9b045ab70691dd1
William <wmario2004@gmail.com>
2025-06-03 17:00:22 -0400

    Add iOS simulator runtime reference to project files for compatibility

--- SplitPayApp/Views/FamilyMembersView.xaml.cs
+++ SplitPayApp/Views/FamilyMembersView.xaml.cs
@@ -2,16 +2,19 @@ namespace SplitPayApp.Views;
 
 public partial class FamilyMembersView : ContentView
 {
-	public FamilyMembersView()
-	{
-		InitializeComponent();
-	}
+    public FamilyMembersView()
+    {
+        InitializeComponent();
+    }
 
     private void AddMemberButton_Clicked(object sender, EventArgs e)
     {
     }
 
     private void UpdateBalanceButton_Clicked(object sender, EventArgs e)
+    {
+    }
+    private void RemoveMemberButton_Clicked(object sender, EventArgs e)
     {
     }
 }
