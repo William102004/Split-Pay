3de58382a81e006e939c976b3c6cbf8a0ca3664b
William <wmario2004@gmail.com>
2025-06-09 15:43:32 -0400

    Refactor ExpenseView and FamilyMembersView for improved functionality; remove obsolete iOS simulator runtime references and update project files.

--- SplitPayApp/Views/ExpenseView.xaml.cs
+++ SplitPayApp/Views/ExpenseView.xaml.cs
@@ -1,9 +1,22 @@
 namespace SplitPayApp.Views;
-
+using SplitPayApp.ViewModels;
 public partial class ExpenseView : ContentView
 {
 	public ExpenseView()
 	{
 		InitializeComponent();
+		BindingContext = new ExpenseViewModel();
 	}
+
+	private void AddExpenseClicked(object sender, EventArgs e)
+	{
+		(BindingContext as ExpenseViewModel)?.AddExpense();
+		(BindingContext as ExpenseViewModel)?.RefreshExpenses();
+    }
+
+	private void RemoveExpenseClicked(object sender, EventArgs e)
+	{
+		(BindingContext as ExpenseViewModel)?.RemoveExpense();
+		(BindingContext as ExpenseViewModel)?.RefreshExpenses();
+    }
 }
--- SplitPayApp/Views/FamilyMembersView.xaml.cs
+++ SplitPayApp/Views/FamilyMembersView.xaml.cs
@@ -1,20 +1,35 @@
 namespace SplitPayApp.Views;
-
+using SplitPayApp.ViewModels;
+using Split_Play_Libraries.Models;
+[QueryProperty(nameof(NewBalance), "newBalance")]
 public partial class FamilyMembersView : ContentView
 {
+
+    public double NewBalance { get; set; }
     public FamilyMembersView()
     {
         InitializeComponent();
+        BindingContext = new FamilyMemberViewModel();
     }
 
     private void AddMemberButton_Clicked(object sender, EventArgs e)
     {
+        (BindingContext as FamilyMemberViewModel)?.AddFamilyMember();
+        (BindingContext as FamilyMemberViewModel)?.refreshFamilyMembers();
     }
 
     private void UpdateBalanceButton_Clicked(object sender, EventArgs e)
     {
+        (BindingContext as FamilyMemberViewModel)?.UpdateMemberBalance(NewBalance);
+        (BindingContext as FamilyMemberViewModel)?.refreshFamilyMembers();
     }
     private void RemoveMemberButton_Clicked(object sender, EventArgs e)
     {
+        (BindingContext as FamilyMemberViewModel)?.RemoveFamilyMember();
+        (BindingContext as FamilyMemberViewModel)?.refreshFamilyMembers();
     }
+    
+    
+    
+    
 }
