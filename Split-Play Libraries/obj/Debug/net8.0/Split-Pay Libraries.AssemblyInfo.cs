9439176f649bbc3c8994c1af4ecb71b0ae923240
William <wmario2004@gmail.com>
2025-06-12 16:06:17 -0400

    Refactor ExpenseViewModel and FamilyMemberViewModel for improved property handling and binding; update related views for better data interaction and UI consistency.

--- Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
+++ Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de28e38a919d901050f4a72781e3a53a82dbdae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d8a7859148032b27e3ae677ae725b153e5652a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
