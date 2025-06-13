6002b1dd5fdf6e54921e45a2be7d02d608e5d4f7
William <wmario2004@gmail.com>
2025-06-13 16:24:33 -0400

    Fixed for the expense view

--- Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
+++ Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+473c6d8abe0cb56ebad2e53c97641459292ac1d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00dd37c53a073c69195bfea7183868d1b79c62f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
