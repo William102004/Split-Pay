599e70157be7d761879eacf1abdffc5979f4ad34
William <wmario2004@gmail.com>
2025-06-23 16:27:35 -0400

    Refactor remove functionality in FamilyMemberViewModel and FamilyMembersView; update DoDelete method to accept a FamilyMember parameter and bind Remove button to the updated method.

--- SplitPayApp/Views/FamilyMembersView.xaml.cs
+++ SplitPayApp/Views/FamilyMembersView.xaml.cs
@@ -1,12 +1,12 @@
 namespace SplitPayApp.Views;
 using SplitPayApp.ViewModels;
 using Split_Play_Libraries.Models;
-
+using Split_Pay_Libraries.Models;
 
 public partial class FamilyMembersView : ContentView
 {
 
-    
+
     public FamilyMembersView()
     {
         InitializeComponent();
@@ -19,17 +19,23 @@ public partial class FamilyMembersView : ContentView
         (BindingContext as FamilyMemberViewModel)?.refreshFamilyMembers();
     }
 
+
     private void UpdateBalanceButton_Clicked(object sender, EventArgs e)
     {
         (BindingContext as FamilyMemberViewModel)?.UpdateMemberBalance();
         (BindingContext as FamilyMemberViewModel)?.refreshFamilyMembers();
     }
-    private void RemoveMemberButton_Clicked(object sender, EventArgs e)
+
+    public void RemoveMemberButton_Clicked(object sender, EventArgs e)
     {
-        //(BindingContext as FamilyMemberViewModel)?.RemoveFamilyMember();
-        (BindingContext as FamilyMemberViewModel)?.DoDelete();
-        (BindingContext as FamilyMemberViewModel)?.refreshFamilyMembers();  
+         if (sender is Button button && button.BindingContext is FamilyMember familyMember)
+        {
+            // Get the view model and call the delete method with the specific family member
+            var viewModel = BindingContext as FamilyMemberViewModel;
+            viewModel?.DoDelete(familyMember);
+        }
     }
+    
     
     
     
