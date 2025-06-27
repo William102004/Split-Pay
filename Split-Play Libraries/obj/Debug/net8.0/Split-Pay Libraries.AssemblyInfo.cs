90daf9d28abf6dd725f3cc04ccca256be583d65c
William <wmario2004@gmail.com>
2025-06-27 16:37:09 -0400

    Update assembly informational version and cache input hash

--- Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
+++ Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b696ff9d9262d57db6a13ca1b7613cd0d4f33bc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57bb50bb3396a1ea17986b18b8e163b19e0b7544")]
 [assembly: System.Reflection.AssemblyProductAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
