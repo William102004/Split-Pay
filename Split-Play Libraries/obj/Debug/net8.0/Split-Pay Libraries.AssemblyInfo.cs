937e44039affe3daf5c044c0952ad95e694ecb3c
William <wmario2004@gmail.com>
2025-07-08 10:47:56 -0400

    Update assembly informational version and adjust project configuration in solution file

--- Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
+++ Split-Play Libraries/obj/Debug/net8.0/Split-Pay Libraries.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91af35044a942f6ed18a3ec3e3c8e3436fbfee7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad6574e25df8891a737fe894aec936f92a0a63e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Split-Pay Libraries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
